d69892fe2b8f31d915cedea09d428fc8967870f9
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-08 16:04:42 -0400

    feat: input validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from "react";
-import InputMask from "react-input-mask";
+import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import { icons } from "./assets";
 import { Col, Container, DropdownButton, Row } from "react-bootstrap";
+import { InputPhoneNumberMask } from "./components/Input";
+import { countries } from "./utils/countries";
 
 function App() {
+
+  const [country, setCountry] = useState("")
+  const [maskCountry, setMaskCountry] = useState("")
+  const [flag, setFlag] = useState("")
+
+  const handleSelectCountry = (e) => {
+    setCountry(e)    
+    handleMask()
+  }
+
+  const handleMask = () => {
+    const data = countries.find((countries) => countries.country === country)
+    setFlag(data.icon)
+    setMaskCountry(`${data.code} ${data.mask}`)
+  }
+
+
+
   return (
     <>
       <Container>
         <Row>
-          <Col xs={1}>
-              <DropdownButton title="country">
-                <Dropdown.Item>Action</Dropdown.Item>
-                <Dropdown.Item>Another action</Dropdown.Item>
-                <Dropdown.Item>Something else</Dropdown.Item>
-              </DropdownButton>
+          <Col xs={4}>
+            <Dropdown onSelect={handleSelectCountry}>
+              <Dropdown.Toggle>
+                {
+                  flag ? <img src={flag} alt={country} /> : "Seleccione un pais"
+                }
+                
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="venezuela">Venezuela</Dropdown.Item>
+                <Dropdown.Item eventKey="colombia">Colombia</Dropdown.Item>
+                <Dropdown.Item eventKey="argentina">Argentina</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Col>
 
-          <Col xs={2}>
-            <InputMask
-              mask="+58 999 999-9999"
-              className="form-control input-color"
+          <Col xs={3}>
+            <InputPhoneNumberMask 
+              mask={maskCountry}
             />
           </Col>
         </Row>
---
+++ src/utils/countries.tsx
@@ -0,0 +1,22 @@
+import { icons } from "./../assets";
+
+export const countries = [
+  {
+    country: "venezuela",
+    icon: icons.venezuela,
+    code: "+58",
+    mask: "999 999 9999",
+  },
+  {
+    country: "colombia",
+    icon: icons.venezuela,
+    code: "+57",
+    mask: "999 9999999",
+  },
+  {
+    country: "argentina",
+    icon: icons.venezuela,
+    code: "+54",
+    mask: "9 999 999 9999",
+  },
+];
