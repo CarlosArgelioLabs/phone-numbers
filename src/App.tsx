f49a33508050dd125a9e3f167f4a9080e2eac845
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-10 16:27:23 -0400

    fix: resolve error select data render first

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import { icons } from "./assets";
-import { Col, Container, DropdownButton, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { InputPhoneNumberMask } from "./components/Input";
 import { countries } from "./utils/countries";
 
@@ -13,14 +12,15 @@ function App() {
 
   const handleSelectCountry = (e) => {
     setCountry(e)    
-    handleMask()
   }
 
-  const handleMask = () => {
+  useEffect(() => {
     const data = countries.find((countries) => countries.country === country)
-    setFlag(data.icon)
-    setMaskCountry(`${data.code} ${data.mask}`)
-  }
+    if ( data !== undefined ) {
+      setFlag(data.icon)
+      setMaskCountry(`${data.code} ${data.mask}`)
+    }
+  }, [country])
 
 
 
