3893c5132fb4b29bc3b2ad3ad8a0099bb0f865b0
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-10 16:15:42 -0400

    fix: resolve error StrictMode production

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { config } from "./../config.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+let REACT_MODE;
+
+if (config.isProd) {
+  REACT_MODE = (
+    <React.Fragment>
+      {" "}
+      <App />{" "}
+    </React.Fragment>
+  );
+} else if (config.isDev) {
+  REACT_MODE = (
+    <React.StrictMode>
+      {" "}
+      <App />{" "}
+    </React.StrictMode>
+  );
+} else {
+  REACT_MODE = (
+    <React.StrictMode>
+      {" "}
+      <App />{" "}
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(REACT_MODE);
